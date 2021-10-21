e41ae03c88a90037fd0c15ccf4a4a6c1576c2f85
Felipe Daiello <fdaiello@misterpostman.com.br>
2021-10-21 17:38:48 -0300

    TestFindClosestValueInBst()

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            TestTournamentWinner();
+            TestFindClosestValueInBst();
         }
 
         // Is Valid Subsequence
@@ -54,6 +54,7 @@ namespace ConsoleApp1
             int[] array = {-10, -2, -1, 2, 3, 5, 6, 8, 9 };
             Console.WriteLine($"Sorted Squared Array: [{string.Join(", ", SortedSquaredArray(array))}]");
         }
+
         // Tournament Winner
         public static string TournamentWinner(List<List<string>> competitions, List<int> results)
         {
@@ -98,6 +99,124 @@ namespace ConsoleApp1
             List<int> results = new List<int> { 0, 0, 1 };
 
             Console.WriteLine($"Tournament Winner: {TournamentWinner(competitions, results)}");
+
+        }
+        // Non Constructible Change
+        public static int NonConstructibleChange(int[] coins)
+        {
+            if (coins.Length == 0)
+                return 1;
+
+            // sort array
+            Array.Sort(coins);
+
+            // if first element is greater than 1, return 1
+            if (coins[0] > 1)
+                return 1;
+
+            // we know whe have 1 coin, so min change starts with 1
+            int minChange = 1;
+
+            // now lets check all coins sorted, and check what is the sum, starting from the second element
+            for ( int x=1; x<coins.Length; x++)
+            {
+
+                if ( minChange + coins[x] > minChange + 1 && coins[x] != minChange + 1 && coins[x] + 1 > minChange + 1)
+                {
+                    return minChange + 1;
+                }
+                else
+                {
+                    minChange += coins[x];
+                }
+            }
+
+            // Write your code here.
+            return minChange + 1;
+
+        }
+        public static void TestNonConstructibleChange()
+        {
+            int[] coins = { 1, 1, 1, 1, 5, 10 };
+
+            Console.WriteLine($"Minimum non constructible change: {NonConstructibleChange(coins)}");
+        }
+
+        // Find Closest Value in BST
+        public static int FindClosestValueInBst(BST tree, int target)
+        {
+            if (tree.value == target)
+                return tree.value;
+            else if (target < tree.value)
+                return FindClosestLeftValueInBst(tree.left, target, tree.value);
+            else
+                return FindClosestRightValueInBst(tree.right, target, tree.value);
+        }
+        public static int FindClosestLeftValueInBst(BST tree, int target, int last)
+        {
+            if (tree.value <= target)
+            {
+                if (Math.Abs(target - tree.value) < Math.Abs(target - last))
+                    return tree.value;
+                else
+                    return last;
+            }
+            else if (target < tree.value)
+                return FindClosestLeftValueInBst(tree.left, target, tree.value);
+            else
+                return FindClosestRightValueInBst(tree.right, target, tree.value);
+        }
+        public static int FindClosestRightValueInBst(BST tree, int target, int last)
+        {
+            if (tree.value >= target)
+            {
+                if (Math.Abs(target - tree.value) < Math.Abs(target - last))
+                    return tree.value;
+                else
+                    return last;
+            }
+            else if (target < tree.value)
+                return FindClosestLeftValueInBst(tree.left, target, tree.value);
+            else
+                return FindClosestRightValueInBst(tree.right, target, tree.value);
+        }
+
+        public class BST
+        {
+            public int value;
+            public BST left;
+            public BST right;
+
+            public BST(int value)
+            {
+                this.value = value;
+            }
+        }
+        public static void TestFindClosestValueInBst()
+        {
+            BST tree = new BST(10)
+            {
+                left = new BST(5)
+                {
+                    left = new BST(2)
+                    {
+                        left = new BST(1)
+                    },
+                    right = new BST(5)
+                },
+                right = new BST(15)
+                {
+                    left = new BST(13)
+                    {
+                        right = new BST(14)
+                    },
+                    right = new BST(22)
+                }
+            };
+
+            int target = 7;
+
+            Console.WriteLine($"Find Closes Value in BST: {FindClosestValueInBst(tree, target)}");
 
         }
     }
